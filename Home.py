695eb1c77eba1ee2a9594a3d06a6733195f88c0c
KevinH <khui98@gmail.com>
2023-11-14 16:04:31 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,7 +13,7 @@ with col_2:
     Hello and welcome to my webpage!
     My name is Kevin Hui, I graduated in 2023 with a Bachelor in Science 
     in Information Technology, Summa Cum Laude, from University of Massachusetts Boston.
-    I have experience with IT troubleshooting and programming language 
+    I have experience with IT troubleshooting and programming languages 
     such as Python, HTML, and Javascript.
     """
     st.write(content)
